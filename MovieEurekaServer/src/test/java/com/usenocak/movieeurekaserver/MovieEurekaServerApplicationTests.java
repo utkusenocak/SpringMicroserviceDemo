d10d9acb237e1c2bda9a11fda82fa00786133d8a
Utku SENOCAK <u.senocakk@gmail.com>
2022-05-24 20:30:23 +0300

    Initialize

---
+++ MovieEurekaServer/src/test/java/com/usenocak/movieeurekaserver/MovieEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usenocak.movieeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
